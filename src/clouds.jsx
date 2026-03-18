065b45e3653286db96ab06a0474ef1e4c1525a5f
Sophia <wwan18@my.bcit.ca>
2026-03-18 01:00:29 -0700

    added text to cloud desc

--- src/clouds.jsx
+++ src/clouds.jsx
@@ -19,7 +19,10 @@ export default function CloudAnimation() {
         <img src="/cloud4.png" alt="" className="cloud-move-left" />
         <img src="/cloud5.png" alt="" className="cloud-move-right" />
       </div>
-      <img className="cloud-desc" src="/Cloud-box.svg" alt="" />
+      <div className="cloud-desc-wrap" aria-hidden="true">
+        <img className="cloud-desc" src="/Cloud-box.svg" alt="" />
+        <div className="cloud-desc-text"><p>Hi, I’m Sophia, a Digital Design and Development student at BCIT. I enjoy combining visual design and front-end development to bring ideas to life. Outside of digital work, I enjoy oil painting and hands-on craft projects.</p></div>
+      </div>
     </div>
   )
 }
